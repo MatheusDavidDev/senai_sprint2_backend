72c4ba9f4326d1bbcd68fc50e382bf2eb32ce493
matheus <matheusviniciusdavid@gmail.com>
2021-04-19 10:25:51 -0300

    criacao da API

--- Exercicios/InLock/bd/inlock_02_DML.sql
+++ Exercicios/InLock/bd/inlock_02_DML.sql
@@ -1,14 +1,26 @@
 USE InLock_Games_Manha
+GO
 
-INSERT INTO Usuarios (titulo, email, senha)
-VALUES				 ('Administrador', 'admin@admin.com', 'admin')
-					 ,('Cliente', 'cliente@cliente.com', 'cliente');
+INSERT INTO TipoUsuarios (titulo)
+VALUES					 ('Administrador')
+						,('Cliente');
+GO
+
+INSERT INTO Usuarios (idTipoUsuario, email, senha)
+VALUES				 (1, 'admin@admin.com', 'admin')
+					,(2, 'cliente@cliente.com', 'cliente');
+GO
 
 INSERT INTO Estudios (nomeEstudio)
 VALUES				 ('Blizzard')
-					  ,('Rockstar Studios')
-					  ,('Square Enix');
+					,('Rockstar Studios')
+					,('Square Enix');
+GO
 
-INSERT INTO Jogos (nomeJogo, descricao, dataLancamento, valor)
-VALUES            ('Diablo 3', 'É um jogo que contém bastante ação e é viciante,seja você um novato ou um fã', '2012/05/15', '99')
-				  ,('Red Dead Redemption II', 'jogo eletrônico de ação-aventura western', '2018/10/26', '120');
+INSERT INTO Jogos (nomeJogo, descricao, dataLancamento, idEstudio, valor)
+VALUES            ('Diablo 3', 'É um jogo que contém bastante ação e é viciante,seja você um novato ou um fã', '2012/05/15', 1, '99')
+				 ,('Red Dead Redemption II', 'jogo eletrônico de ação-aventura western', '2018/10/26', 2, '120');
+GO
+
+
+
